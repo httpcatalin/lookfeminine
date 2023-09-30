db6c44e25343a727f51a0d83c020b12397ac4f24
Catalin <bitca.catalin@gmail.com>
2023-10-01 01:19:03 +0300

    host16

--- lookfeminine/settings.py
+++ lookfeminine/settings.py
@@ -137,14 +137,5 @@ USE_TZ = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-# settings.py
 
 
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-MEDIA_URL = 'media/'
