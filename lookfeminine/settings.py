8fe3070b0ecc9da58add1bc9288d8e7ae7fc25a9
Catalin <bitca.catalin@gmail.com>
2023-10-01 01:27:33 +0300

    host16

--- lookfeminine/settings.py
+++ lookfeminine/settings.py
@@ -137,5 +137,10 @@ USE_TZ = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
+# settings.py
 
+# Static files (CSS, JavaScript, images)
+STATIC_URL = '/static/'
 
+# Directory where collectstatic will place static files for deployment
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
